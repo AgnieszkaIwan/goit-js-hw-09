cae00a55868d8ab9b0992e03c429ad9455f866b4
Agnieszka Iwan <agnieszka.iwan2@gmail.com>
2023-05-04 22:49:20 +0200

    commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,20 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+});
